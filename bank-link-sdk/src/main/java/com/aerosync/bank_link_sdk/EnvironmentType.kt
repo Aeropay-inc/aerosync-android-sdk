13a5a682100145463aec887027cbab05580de6b4
jayon <jayon.niravel@aeropay.com>
2024-07-03 11:57:17 -0500

    updated Aerosync URLs to be under the domain *.aero.inc

--- bank-link-sdk/src/main/java/com/aerosync/bank_link_sdk/EnvironmentType.kt
+++ bank-link-sdk/src/main/java/com/aerosync/bank_link_sdk/EnvironmentType.kt
@@ -1,7 +1,7 @@
 package com.aerosync.bank_link_sdk
 
 enum class EnvironmentType(val value: String) {
-    DEV("https://qa.aerosync.com"),
-    STAGE("https://staging.aerosync.com"),
-    PROD("https://www.aerosync.com")
+    DEV("https://qa-sync.aero.inc"),
+    STAGE("https://staging-sync.aero.inc"),
+    PROD("https://sync.aero.inc")
 }
