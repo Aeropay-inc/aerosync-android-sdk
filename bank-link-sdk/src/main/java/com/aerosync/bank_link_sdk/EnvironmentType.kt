48edab736570971a9f5ae95187ddd6da75a58184
jniravel <133315298+jniravel@users.noreply.github.com>
2024-10-17 11:18:54 -0500

    added sandbox value (#5)

--- bank-link-sdk/src/main/java/com/aerosync/bank_link_sdk/EnvironmentType.kt
+++ bank-link-sdk/src/main/java/com/aerosync/bank_link_sdk/EnvironmentType.kt
@@ -3,5 +3,6 @@ package com.aerosync.bank_link_sdk
 enum class EnvironmentType(val value: String) {
     DEV("https://dev.aerosync.com"),
     STAGE("https://staging.aerosync.com"),
+    SANDBOX("https://sandbox.aerosync.com"),
     PROD("https://www.aerosync.com")
 }
